c3094c8a22e71070cde12d85a38ad875dfc15a3c
Jacqueline Chenault <adigitalnative@gmail.com>
2018-09-18 20:24:32 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+require 'pry'
+
+class School
+  attr_reader :roster
+
+  def initialize(name)
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    # Either add student to existing grade or create a new grade with the student.
+    @roster.keys.include?(grade) ? @roster[grade] << name : @roster[grade] = [name]
+  end
+
+  def grade(grade_to_find)
+    @roster[grade_to_find]
+  end
+
+  def sort
+    @roster.each do |grade, names|
+      names.sort!
+    end
+  end
+
+end
